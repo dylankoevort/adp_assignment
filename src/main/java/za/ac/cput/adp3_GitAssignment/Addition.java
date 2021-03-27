b93944d708078882c6040fe5ef8830a9c65e5c44
Jonathan <HitsLikeTruck@users.noreply.github.com>
2021-03-27 18:13:40 +0200

    Added simple code to Addition program

--- src/main/java/za/ac/cput/adp3_GitAssignment/Addition.java
+++ src/main/java/za/ac/cput/adp3_GitAssignment/Addition.java
@@ -1,10 +1,21 @@
 package za.ac.cput.adp3_GitAssignment;
 
+
 /**
  *
- * small addition program
- *
+ * Small addition program
+ * Jonathan Kleynhans
+ * 217268153
  * **/
 
 public class Addition {
+
+    public static void integerSum(){
+        int a = 5;
+        int b = 10;
+        int results = a + b;
+
+        System.out.println(results);
+    }
+
 }
