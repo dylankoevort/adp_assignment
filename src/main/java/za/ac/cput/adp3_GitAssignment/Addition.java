95f52fcc846c60614c33aefe48bbd268161130c5
Jonathan <HitsLikeTruck@users.noreply.github.com>
2021-03-31 11:44:41 +0200

    Updated comments

--- src/main/java/za/ac/cput/adp3_GitAssignment/Addition.java
+++ src/main/java/za/ac/cput/adp3_GitAssignment/Addition.java
@@ -1,9 +1,9 @@
 package za.ac.cput.adp3_GitAssignment;
 
 /**
- *
- * small addition program
- *
+ * Small addition program
+ * @author Jonathan Kleynhans
+ * 217268153
  * **/
 
 public class Addition {
